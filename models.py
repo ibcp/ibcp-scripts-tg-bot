1a35b1c9ed1f4cbedfd9c2fbf7d7428d8eeec099
rguliev <glvrst@gmail.com>
2019-07-30 19:28:14 +0300

    Add pre-commit hooks: black + flake8 + mypy

--- models.py
+++ models.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class UserFiles(db.Model):
-    __tablename__ = 'userfiles'
+
+class UserFiles(db.Model):  # type: ignore
+    __tablename__ = "userfiles"
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
@@ -14,14 +15,16 @@ class UserFiles(db.Model):
     file_name = db.Column(db.String(64), nullable=False)
 
     def __str__(self):
-        return json.dumps({
-            "id": self.id,
-            "user_id": self.user_id,
-            "chat_id": self.chat_id,
-            "message_id": self.message_id,
-            "file_id": self.file_id,
-            "file_name": self.file_name
-            })
+        return json.dumps(
+            {
+                "id": self.id,
+                "user_id": self.user_id,
+                "chat_id": self.chat_id,
+                "message_id": self.message_id,
+                "file_id": self.file_id,
+                "file_name": self.file_name,
+            }
+        )
 
     def __repr__(self):
         return str(self)
--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ import logging
 import zipfile
 from pyunpack import Archive
 
+
 def remove_extension(path):
     return os.path.splitext(path)[0]
+
 
 def get_file_info(bot, userfile_id):
     from app import app, db
@@ -22,57 +24,66 @@ def get_file_info(bot, userfile_id):
         "file": file,
         "message_id": file_message_id,
         "filename": filename,
-        "file_extension": filename.split('.')[-1],
+        "file_extension": filename.split(".")[-1],
         "download_path": os.path.join(
-            app.config['DOWLOAD_DIR'],
-            '%s %s' % (userfile_id, filename)
-            ),
-        "extract_path": remove_extension(os.path.join(
-            app.config['TMP_DIR'],
-            '%s %s' % (userfile_id, filename)
-            )),
+            app.config["DOWLOAD_DIR"], "%s %s" % (userfile_id, filename)
+        ),
+        "extract_path": remove_extension(
+            os.path.join(
+                app.config["TMP_DIR"], "%s %s" % (userfile_id, filename)
+            )
+        ),
     }
 
+
 def extract_file(bot, chat_id, file_info):
-    if file_info['file_extension'] not in ('zip', 'rar', 'txt'):
+    if file_info["file_extension"] not in ("zip", "rar", "txt"):
         logging.error("Incorrect file extension.")
         bot.send_message(
             chat_id=chat_id,
-            text="Выбранный формат файла не поддерживается." + \
-            " Пришлите, пожалуйста, файл в одном из следующих раширений: *.txt, *.zip, *.rar"
+            text="Выбранный формат файла не поддерживается."
+            + " Пришлите, пожалуйста, файл в одном из следующих раширений: *.txt, *.zip, *.rar",
+        )
+        raise ValueError(
+            "Unsupported file format: %s." % file_info["file_extension"]
+        )
+
+    file_info["file"].download(custom_path=file_info["download_path"])
+
+    if not os.path.exists(file_info["extract_path"]):
+        os.makedirs(file_info["extract_path"])
+
+    if file_info["file_extension"] == "txt":
+        file_info["file"].download(
+            custom_path=os.path.join(
+                file_info["extract_path"], file_info["filename"]
             )
-        raise ValueError("Unsupported file format: %s." % file_info['file_extension'])
-
-    file_info['file'].download(custom_path=file_info['download_path'])
-
-    if not os.path.exists(file_info['extract_path']):
-        os.makedirs(file_info['extract_path'])
-
-    if file_info['file_extension'] == 'txt':
-        file_info['file'].download(custom_path=os.path.join(file_info['extract_path'], file_info['filename']))
-    elif file_info['file_extension'] in ('zip', 'rar'):
+        )
+    elif file_info["file_extension"] in ("zip", "rar"):
         try:
-            Archive(file_info['download_path']).extractall(file_info['extract_path'])
+            Archive(file_info["download_path"]).extractall(
+                file_info["extract_path"]
+            )
         except Exception as e:
             logging.error(e)
             bot.send_message(
                 chat_id=chat_id,
                 text=[
                     "Упс! Не удалось распаковать архив \U0001F631",
-                    "Проверьте, что файл в правильном формате, если так," + \
-                    " то передайте следующую информацию администратору, чтобы он все исправил:",
-                    "Error on unpacking file. User file: %s" % (file_info['userfile_id'], )
-                    ].join("\n")
-                )
+                    "Проверьте, что файл в правильном формате, если так,"
+                    + " то передайте следующую информацию администратору, чтобы он все исправил:",
+                    "Error on unpacking file. User file: %s"
+                    % (file_info["userfile_id"],),
+                ].join("\n"),
+            )
             raise
-    return 'OK'
+    return "OK"
+
 
 def zipdir(path, out):
-    zipf = zipfile.ZipFile(out, 'w', zipfile.ZIP_DEFLATED)
+    zipf = zipfile.ZipFile(out, "w", zipfile.ZIP_DEFLATED)
     for root, dirs, files in os.walk(path):
         for file in files:
             file_path = os.path.join(root, file)
             zipf.write(file_path, os.path.relpath(file_path, path))
     zipf.close()
-
-
